fb241c50936da0b9dc7976c27ea530ff0ebf10d5
endbstns620-design <endbstns620@gmail.com>
2026-09-05 21:48:46 +0900

    Add files via upload

--- src/components/MainHomeSection.tsx
+++ src/components/MainHomeSection.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 // 요청하신 메인화면 시안을 그대로 표시합니다.
 // 시안 상단의 내비게이션 부분은 이미지에서 제외해 실제 HeaderNavbar의 탭 기능은 그대로 유지됩니다.
-const MAIN_HOME_REFERENCE_IMAGE = '/images/homepage-main-reference.png';
+const MAIN_HOME_REFERENCE_IMAGE = '/images/homepage-main-reference.jpg';
 
 export const MainHomeSection: React.FC = () => {
   return (
